6428dbf8e3625fdda6f3a3f2cfa96dd12988d2a3
Shubham <shubham@Shubhams-MacBook.local>
2022-11-21 13:45:54 +0530

    initial commit

---
+++ src/main/java/com/example/oopsprojectbackend/OopsProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopsprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopsProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopsProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oopsprojectbackend/OopsProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopsprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopsProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
